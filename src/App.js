d0b5c0e8c86f73d0364974cd74ae47a5a8d30641
Lolu Bolarinwa <lolubolarinwa@gmail.com>
2023-08-04 22:41:13 +0100

    reactjs brushup

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ function App() {
         </form>
         <div className="image">
           <img src=""  alt="AI generated Image" />
+          <img src=""  alt="AI generated Image" />
         </div>
 
       </div>
